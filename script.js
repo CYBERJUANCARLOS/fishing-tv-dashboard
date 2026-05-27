730fa02ac7918d4494c60328f723af2d89e4b56b
JUANCARLO$ <114345010+CYBERJUANCARLOS@users.noreply.github.com>
2026-05-27 18:09:42 +0200

    Refactor fishing info and ticker loading functions

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-async function loadFishingInfo() {
-  const response = await fetch("fishing.json");
-  const data = await response.json();
+var fishingData = [
+  {
+    titel: "JETZT ANGELN",
+    text: "Bester Spot: Schilfkante am See<br>Zielfisch: Hecht / Barsch<br>Beste Zeit: 19:00–22:00<br>Köder: Spinner oder Gummifisch<br>Fangchance: Hoch"
+  },
+  {
+    titel: "WETTERLAGE",
+    text: "Bewölkt, leichter Wind. Gute Bedingungen für Hecht in flachen Bereichen."
+  },
+  {
+    titel: "SPOT-TIPP",
+    text: "Erst flache Uferzonen testen, danach tiefere Kanten absuchen."
+  }
+];
 
-  const container = document.getElementById("fishingInfo");
+var tickerData = [
+  "🎣 Beste Angelzeit heute: 19:00–22:00",
+  "📍 Erst die flachen Schilfkanten testen",
+  "🐟 Zielfisch: Hecht und Barsch",
+  "🌦️ Windrichtung vor Spotwechsel prüfen"
+];
+
+function loadFishingInfo() {
+  var container = document.getElementById("fishingInfo");
   container.innerHTML = "";
 
-  data.forEach(item => {
-    container.innerHTML += `
-      <div class="card">
-        <strong>${item.titel}</strong><br><br>
-        ${item.text}
-      </div>
-    `;
-  });
+  for (var i = 0; i < fishingData.length; i++) {
+    container.innerHTML +=
+      '<div class="card">' +
+      '<strong>' + fishingData[i].titel + '</strong><br><br>' +
+      fishingData[i].text +
+      '</div>';
+  }
 }
 
-async function loadTicker() {
-  const response = await fetch("ticker.json");
-  const data = await response.json();
-
-  const ticker = document.getElementById("bottomTicker");
-  ticker.innerHTML = data.join(" &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp; ");
-
-  data.forEach(text => {
-    ticker.innerHTML += `<div class="tickerItem">${text}</div>`;
-  });
+function loadTicker() {
+  var ticker = document.getElementById("bottomTicker");
+  ticker.innerHTML = tickerData.join(" &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp; ");
 }
 
 loadFishingInfo();
 loadTicker();
-
-setInterval(() => {
-  loadFishingInfo();
-  loadTicker();
-}, 60000);
